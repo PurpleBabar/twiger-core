5a10c6da13e05cebfb14826740c09f6dff4bf004
BabarMajes <al@neomajes.com>
2015-04-10 11:47:00 +0200

    Add travis support

--- lib/Router.php
+++ lib/Router.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace twiger;
 
 class Router{
---
+++ tests/RouterTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace twiger;
+
+use twiger\Router as Router;
+
+class RouterTest extends PHPUnit_Framework_TestCase{
+	
+	private $_routes;
+	private $_router;
+
+	public function setUp(){
+		$this->_routes = array('home' => array('pattern' => '/lol/mdr', 'template' => '/mdr/pwet'),
+						'bis' => array('pattern' => '/xd/ptdr', 'template' => '/ptdr/asap'),
+						'ter' => array('pattern' => '/afk/vip', 'template' => '/vip/ppda')); 
+		$this->_router = new Router($this->_routes);
+	}
+
+	public function tearDown(){ 
+		unset($this->_routes);
+		unset($this->_router);
+	}
+
+	public function testHandle(){
+
+		$requesUri="/vip/ppda";
+		$this->AssertType('array', $this->_router->handle($requesUri));
+
+	}
+
+	public function testMatcher(){
+
+		$requesUri="/vip/ppda";
+		$this->AssertType('string', $this->_router->matcher($requesUri));
+
+	}
+
+	public function testParams(){
+
+		$route = array('home' => array('pattern' => '/lol/mdr', 'template' => '/mdr/pwet'));
+		$requesUri="/vip/ppda";
+		$this->AssertType('array', $this->_router->params($route, $requesUri, 'try'));
+
+	}
+
+}
